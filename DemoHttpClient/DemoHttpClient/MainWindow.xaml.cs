83476577f1783a30b6113c239fe2010443cc8280
MinhNK251 <nguyenkhanhminh251@gmail.com>
2025-02-05 13:08:48 +0700

    Initial commit of .NET projects

---
+++ DemoHttpClient/DemoHttpClient/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System;
+
+namespace DemoHttpClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        readonly HttpClient client = new HttpClient();
+        private async void btnView_Click(object sender, RoutedEventArgs e)
+        {
+            string uri = txtURL.Text;
+            try
+            {
+                string response = await client.GetStringAsync(uri);
+                txtSource.Text = response.Trim();
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show($"Exception : {ex.Message}");
+            }
+        }
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtSource.Text = string.Empty;
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ NetworkingSample/NetworkingSample/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+
+namespace NetworkingSample
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //URIDemo();
+      //WebClientDemo();
+      HttpClientDemo();
+      Console.ReadLine();
+    }
+
+    private static async void HttpClientDemo()
+    {
+      try
+      {
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync("http://google.com");
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(responseBody);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("\nException Caught!");
+        Console.WriteLine("Message: " + ex.Message);
+      }
+    }
+
+    private static void WebClientDemo()
+    {
+      WebRequest request = WebRequest.Create("http://google.com");
+      request.Credentials = CredentialCache.DefaultCredentials;
+      HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+      Console.WriteLine("Status: " + response.StatusDescription);
+      Console.WriteLine(new string('*', 50));
+
+      Stream dataStream = response.GetResponseStream();
+      StreamReader reader = new StreamReader(dataStream);
+      string responseFromServer = reader.ReadToEnd();
+      Console.WriteLine(responseFromServer);
+      Console.WriteLine(new string('*', 50));
+      reader.Close();
+      dataStream.Close();
+      response.Close();
+    }
+
+    private static void URIDemo()
+    {
+      Uri info = new Uri("http://www.domain.com:80/info?id=123#top5");
+      Uri page = new Uri("http://www.domain.com/info/page.html");
+      Console.WriteLine("Host: " + info.Host);
+      Console.WriteLine("Port: " + info.Port);
+      Console.WriteLine("PathAndQuery: " + info.PathAndQuery);
+      Console.WriteLine("Query: " + info.Query);
+      Console.WriteLine("Fragment: " + info.Fragment);
+      Console.WriteLine("Default HTTP Port: " + info.Port);
+      Console.WriteLine("IsBaseOf: " + info.IsBaseOf(page));
+
+      Uri relative = info.MakeRelativeUri(page);
+      Console.WriteLine("IsAbsoluteUri: " + relative.IsAbsoluteUri);
+      Console.WriteLine("RelativeUri: " + relative.ToString());
+    }
+  }
+}
