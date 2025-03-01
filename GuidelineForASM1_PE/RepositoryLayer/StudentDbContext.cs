2dc9c07e1dc6a1fb8b15b7efd4c492b64a0fa7da
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-03-01 12:28:15 +0700

    Add Guideline For ASM1

---
+++ GuidelineForASM1_PE/RepositoryLayer/StudentDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Models;
+
+namespace RepositoryLayer
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Student> Students { get; set; }
+    } 
+}
---
+++ GuidelineForASM1_PE/RepositoryLayer/StudentRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Models;
+
+namespace RepositoryLayer
+{
+    public class StudentRepository
+    {
+        private readonly StudentDbContext _dbContext;
+        public StudentRepository(StudentDbContext dbContext) {
+            _dbContext = dbContext;
+        }
+        public Student Get(int id)
+        {
+            return _dbContext.Students.FirstOrDefault(m => m.Id == id);
+        }
+    }
+}
