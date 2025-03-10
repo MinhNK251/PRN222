f1b4df7f1bd86e79576f4d55a8ce7053de0b73dd
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-03-10 16:53:48 +0700

    Add CRUD pages for Category

---
+++ NguyenKhanhMinh_NET1707_A02/NguyenKhanhMinhRazorPages/Pages/CategoryPages/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using BusinessObjectsLayer.Models;
+using DAOsLayer;
+
+namespace NguyenKhanhMinhRazorPages.Pages.CategoryPages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DAOsLayer.FunewsManagementContext _context;
+
+        public CreateModel(DAOsLayer.FunewsManagementContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["ParentCategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryDesciption");
+            return Page();
+        }
+
+        [BindProperty]
+        public Category Category { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Categories.Add(Category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
