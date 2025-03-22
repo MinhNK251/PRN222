c3d213479c0bc606ef1b763f5458e7b88877bc08
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-03-22 14:51:28 +0700

    Add BusinessObject

---
+++ PE_PRN222_SP25_TrialTest_NguyenKhanhMinh/BusinessObjectLayer/CustomValidation/ValidateActiveIngredients.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace BusinessObjectLayer.CustomValidation
+{
+    public class ValidateActiveIngredients : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return new ValidationResult("ActiveIngredients is required.");
+            }
+
+            string activeIngredients = value.ToString()!;
+
+            // Check if length is greater than 10
+            if (activeIngredients.Length <= 10)
+            {
+                return new ValidationResult("ActiveIngredients must be greater than 10 characters.");
+            }
+
+            // Split into words and check each word
+            string[] words = activeIngredients.Split(' ');
+            foreach (var word in words)
+            {
+                if (!Regex.IsMatch(word, @"^[A-Z0-9][a-zA-Z0-9]*$"))
+                {
+                    return new ValidationResult("Each word in ActiveIngredients must begin with a capital letter or a number and contain no special characters.");
+                }
+            }
+
+            // Check for forbidden special characters
+            if (Regex.IsMatch(activeIngredients, @"[#@&()]"))
+            {
+                return new ValidationResult("ActiveIngredients must not contain special characters such as #, @, &, (, ).");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ PE_PRN222_SP25_TrialTest_NguyenKhanhMinh/BusinessObjectLayer/MedicineInformation.cs
@@ -0,0 +1,26 @@
+﻿using BusinessObjectLayer.CustomValidation;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessObjectLayer;
+
+public partial class MedicineInformation
+{
+    [Required]
+    public string MedicineId { get; set; } = null!;
+    [Required]
+    public string MedicineName { get; set; } = null!;
+
+    [Required]
+    [ValidateActiveIngredients]
+    public string ActiveIngredients { get; set; } = null!;
+    [Required]
+    public string? ExpirationDate { get; set; }
+    [Required]
+    public string DosageForm { get; set; } = null!;
+    [Required]
+    public string WarningsAndPrecautions { get; set; } = null!;
+    [Required]
+    public string? ManufacturerId { get; set; }
+
+    public virtual Manufacturer? Manufacturer { get; set; }
+}
