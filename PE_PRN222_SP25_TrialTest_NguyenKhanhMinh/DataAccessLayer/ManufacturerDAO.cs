4eddb8ffeae7f1db702b7c18d9873f61881fbfb7
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-03-22 14:51:44 +0700

    Add DataAccess

---
+++ PE_PRN222_SP25_TrialTest_NguyenKhanhMinh/DataAccessLayer/ManufacturerDAO.cs
@@ -0,0 +1,41 @@
+﻿using BusinessObjectLayer;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer
+{
+    public class ManufacturerDAO
+    {
+        private Sp25PharmaceuticalDbContext _dbContext;
+        private static ManufacturerDAO instance;
+
+        public ManufacturerDAO()
+        {
+            _dbContext = new Sp25PharmaceuticalDbContext();
+        }
+
+        public static ManufacturerDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ManufacturerDAO();
+                }
+                return instance;
+            }
+        }
+
+        private Sp25PharmaceuticalDbContext CreateDbContext()
+        {
+            return new Sp25PharmaceuticalDbContext();
+        }
+
+        public List<Manufacturer> GetManufacturers()
+        {
+            using (var dbContext = CreateDbContext())
+            {
+                return dbContext.Manufacturers.AsNoTracking().Include(manufacturer => manufacturer.MedicineInformations).ToList();
+            }
+        }
+    }
+}
