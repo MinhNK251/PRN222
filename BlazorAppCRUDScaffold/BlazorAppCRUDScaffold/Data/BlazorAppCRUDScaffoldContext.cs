66d7aba2ade1d2e0fa5acf46bb2896c800f3ab6b
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-03-29 12:45:06 +0700

    Add BlazorAppCRUDScaffold

---
+++ BlazorAppCRUDScaffold/BlazorAppCRUDScaffold/Data/BlazorAppCRUDScaffoldContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorAppCRUDScaffold;
+
+namespace BlazorAppCRUDScaffold.Data
+{
+    public class BlazorAppCRUDScaffoldContext : DbContext
+    {
+        public BlazorAppCRUDScaffoldContext (DbContextOptions<BlazorAppCRUDScaffoldContext> options)
+            : base(options)
+        {
+          this.Database.EnsureCreated();
+        }
+
+        public DbSet<BlazorAppCRUDScaffold.Student> Student { get; set; } = default!;
+    }
+}
