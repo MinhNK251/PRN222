850f3995c318b3565e5b2ead4c48275505975cd8
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-04-08 22:51:24 +0700

    Add DTOs pt.3

---
+++ PRN222_ASM3/DataAccess/DTO/ProductSignalRDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace DataAccess.DTO
+{
+    public class ProductSignalRDTO
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string Weight { get; set; } = string.Empty;
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+}
