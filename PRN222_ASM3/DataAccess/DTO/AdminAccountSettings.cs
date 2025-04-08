1cbfdd483035c8c7e69ff24e37c802d04f910dca
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-04-08 22:50:57 +0700

    Add DTOs pt.1

---
+++ PRN222_ASM3/DataAccess/DTO/AdminAccountSettings.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace DataAccess.DTO;
+
+public class AdminAccountSettings
+{
+	public string Email { get; set; }
+	public string Password { get; set; }
+}
