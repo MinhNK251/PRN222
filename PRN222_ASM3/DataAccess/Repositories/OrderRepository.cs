d853f349f03828572db05178f9f84400dd4f4b0d
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-04-09 23:55:29 +0700

    Add Repository

---
+++ PRN222_ASM3/DataAccess/Repositories/OrderRepository.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessObject.Entities;
+using DataAccess.Data;
+using DataAccess.DTO;
+using DataAccess.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly eStoreDuckContext _context;
+
+        public OrderRepository(eStoreDuckContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddOrderAsync(Order order)
+        {
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteOrderAsync(int orderId)
+        {
+            var order = await _context.Orders
+                .Include(o => o.OrderDetails)
+                .FirstOrDefaultAsync(o => o.OrderId == orderId);
+            if (order != null)
+            {
+                if (order.OrderDetails != null && order.OrderDetails.Any())
+                {
+                    _context.OrderDetails.RemoveRange(order.OrderDetails);
+                }
+                _context.Orders.Remove(order);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<Order>> GetAllOrdersAsync()
+        {
+            return await _context.Orders
+                .Include(o => o.OrderDetails)
+                .Include(o => o.Member)
+                .ToListAsync();
+        }
+
+        public async Task<Order> GetOrderByIdAsync(int orderId)
+        {
+            return await _context.Orders
+                .Include(o => o.OrderDetails)
+                .Include(o => o.Member)
+                .FirstOrDefaultAsync(o => o.OrderId == orderId);
+        }
+
+        public async Task UpdateOrderAsync(Order order)
+        {
+            _context.Orders.Update(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Order>> GetOrdersByMemberIdAsync(int memberId)
+        {
+            return await _context.Orders
+                .Include(o => o.OrderDetails)
+                .Include(o => o.Member)
+                .Where(o => o.MemberId == memberId)
+                .ToListAsync();
+        }
+
+        public async Task<List<SalesReportItemDTO>> GetSalesReportAsync(DateTime startDate, DateTime endDate)
+        {
+            var orders = await _context.Orders
+                .Include(o => o.OrderDetails)
+                .Include(o => o.Member)
+                .Where(o => o.OrderDate >= startDate && o.OrderDate <= endDate)
+                .ToListAsync();
+
+            return orders.Select(o => new SalesReportItemDTO
+            {
+                OrderId = o.OrderId,
+                OrderDate = o.OrderDate,
+                MemberCompanyName = o.Member?.CompanyName ?? "Unknown",
+                TotalAmount = o.OrderDetails.Sum(od => od.UnitPrice * od.Quantity * (1 - (decimal)od.Discount))
+            })
+            .OrderByDescending(o => o.TotalAmount)
+            .ToList();
+        }
+
+        public async Task<decimal> GetTotalSalesAsync(DateTime startDate, DateTime endDate)
+        {
+            var orders = await _context.Orders
+                .Include(o => o.OrderDetails)
+                .Where(o => o.OrderDate >= startDate && o.OrderDate <= endDate)
+                .ToListAsync();
+
+            return orders.Sum(o => o.OrderDetails.Sum(od => od.UnitPrice * od.Quantity * (1 - (decimal)od.Discount)));
+        }
+
+        public async Task<int> GetTotalOrdersAsync(DateTime startDate, DateTime endDate)
+        {
+            return await _context.Orders
+                .Where(o => o.OrderDate >= startDate && o.OrderDate <= endDate)
+                .CountAsync();
+        }
+    }
+}
